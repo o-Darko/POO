fd77337dd74e113a437f1dc132396fe23da247aa
o-Darko <yebodan14@gmail.com>
2026-04-26 03:17:39 +0000

    Add files via upload

--- test.php
+++ test.php
@@ -1,1 +1,8 @@
-
+<?php
+// on va faire un test pour voir si on peut faire upload un fichier
+if (isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+    $uploadDirectory = 'uploads/'; // Assurez-vous que ce répertoire existe et est accessible en écriture
+}
+?><!DOCTYPE html>
+<html lang="en">
